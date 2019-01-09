c0d387eb324267f1162243e5c3b36f6a02a6fe33
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-01-09 23:54:49 +0000

    setup testing framework CU. add tests for tokenization. add documentation and script to install and make CU.

---
+++ code/tests/runTests.c
@@ -0,0 +1,17 @@
+// Include definition of T1 test suite
+#include "suites/test_prompt_tokenize.h"
+
+// This struct contains all test suites
+// We plan to test as much of this code as we can, so each test suite will be a separate directory.
+TEST_SUITES {
+    TEST_SUITE_ADD(T1), // add T1 test suite
+    TEST_SUITES_CLOSURE
+};
+
+int main(int argc, char *argv[]) {
+    // set up directory where are stored files with outputs from test suites
+    CU_SET_OUT_PREFIX("regressions/");
+    // run all test suites
+    CU_RUN(argc, argv);
+    return 0;
+}
---
+++ code/tests/suites/test_prompt_tokenize.h
@@ -0,0 +1,11 @@
+#include "../cu/cu.h"
+// Declarations of tests
+TEST(test1);
+TEST(test2);
+
+// Collect tests into test suite
+TEST_SUITE(T1) {
+    TEST_ADD(test1),
+    TEST_ADD(test2),
+    TEST_SUITE_CLOSURE
+};
