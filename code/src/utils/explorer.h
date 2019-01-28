9ab886be12437b7943c51246a620efbdc03a5267
Nohclu <connor.mulready2@mail.dcu.ie>
2019-01-28 13:03:24 +0000

    add explorer ls, cd

---
+++ code/src/utils/explorer.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void expls(FILE *fp, char * command, char **currdir){
+    
+    char path[1024];
+    fp = popen(command, "r");
+    if (fp == NULL){
+        exit(1);
+    }
+
+    int i = 0;
+    while(fgets(path, sizeof(path)-1, fp) != NULL){
+        strcpy(currdir[i],path);
+        i++;
+    }
+    pclose(fp);
+    free(fp);
+}
+
+void expcd(char * dir){
+    chdir(dir);
+}
