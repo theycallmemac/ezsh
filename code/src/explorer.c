eed8d5a435dda48ba36e1c2e5a97715dc27746d5
Nohclu <connor.mulready2@mail.dcu.ie>
2019-02-06 12:50:57 +0000

    Add check file type

--- code/src/explorer.c
+++ code/src/explorer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncurses.h>
 #include "./utils/explorer.h"
 
 int main()
@@ -10,6 +11,7 @@ int main()
     char **currdir;
     char userInput[1];
 
+    initscr();
     while(1){
         scanf("%s", userInput);
         
@@ -25,13 +27,22 @@ int main()
             strcpy(command, "/bin/ls");
             expls(fptr, command, currdir);
             for(int i=0; i<100; i++){
-            printf("%s", currdir[i]);
+                printf("%s", currdir[i]);
             }
         }
         else if(strncmp("cd",userInput,2)==0){
-            char userDir[50];
+            char userDir[100];
             scanf("%s",userDir);
-            expcd(userDir);
+            
+            /*If directory chdir*/
+            if(isDir(userDir) == 1){
+                printf("Is dir: %d\n",isDir(userDir));
+                chdir(userDir);
+            }
+            /*If file do nothing */
+            else if(isFile(userDir) == 1){
+                printf("Is file: %d\n",isFile(userDir));
+            }
         }
         else {
             printf("Nothing hit\n");
--- code/src/utils/explorer.h
+++ code/src/utils/explorer.h
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include <string.h>
 #include <unistd.h>
-/* Input: FILE *ptr, bash command, string[]
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* 
+Input: FILE *ptr, bash command, string[]
 Output: Void, pass by reference  
 */
 void expls(FILE *fp, char * command, char **currdir){
@@ -21,7 +28,41 @@ void expls(FILE *fp, char * command, char **currdir){
     pclose(fp);
     free(fp);
 }
-
+/* chdir alias
+Input: char* directory
+*/
 void expcd(char * dir){
     chdir(dir);
+}
+
+/*
+Input: file name
+Output: bool
+*/
+_Bool isFile(const char *name)
+{
+    struct stat file_stat;
+    stat(name, &file_stat);
+    int fileType = S_ISREG(file_stat.st_mode);
+    if(fileType == 1){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/*
+Input: file name
+Output: bool
+*/
+_Bool isDir(const char *name)
+{
+    struct stat dir_stat;
+    stat(name, &dir_stat);
+    int fileType = S_ISDIR(dir_stat.st_mode);
+    if(fileType == 1){
+        return 1;
+    } else {
+        return 0;
+    }
 }
