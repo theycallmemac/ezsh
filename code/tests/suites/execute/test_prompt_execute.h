6c4915fcd754a374c21ea5e2369a421189e13b39
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-02-06 16:37:56 +0000

    Revert "implemented history command closes #8"
    This reverts commit 5b7eb754f4085c91f678d2f9788b51960f2585c0

--- code/tests/suites/execute/test_prompt_execute.h
+++ code/tests/suites/execute/test_prompt_execute.h
@@ -1,13 +1,13 @@
 #include "../../cu/cu.h"
 // Declarations of tests
+TEST(test7);
 TEST(test8);
 TEST(test9);
-TEST(test10);
 
 // Collect tests into test suite
 TEST_SUITE(T3) {
+    TEST_ADD(test7),
     TEST_ADD(test8),
     TEST_ADD(test9),
-    TEST_ADD(test10),
     TEST_SUITE_CLOSURE
 };
