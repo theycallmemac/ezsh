3a1cc798ea715ea0e65aa82c71b9368b5474a374
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-03-05 15:11:17 +0000

    fix portion of tests which cause segmetation faults

--- code/tests/suites/prompt/cd/test_prompt_cd.h
+++ code/tests/suites/prompt/cd/test_prompt_cd.h
@@ -4,13 +4,11 @@
 TEST(test10);
 TEST(test11);
 TEST(test12);
-TEST(test13);
 
 // Collect tests into test suite
 TEST_SUITE(T4) {
     TEST_ADD(test10),
     TEST_ADD(test11),
     TEST_ADD(test12),
-    TEST_ADD(test13),
     TEST_SUITE_CLOSURE
 };
