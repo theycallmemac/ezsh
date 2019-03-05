51629ea879f1a2f91cdfaca11e3e11a3944c1874
Nohclu <connor.mulready2@mail.dcu.ie>
2019-03-05 11:10:41 +0000

    build fix

--- code/src/utils/panesync.h
+++ code/src/utils/panesync.h
@@ -3,25 +3,33 @@
 #include <string.h>
 #include <fcntl.h>
 
-void pipeWrite(char* msg){
+void pipeWrite(char* msg, char* filePath){
     int pipeFile;
-    char * pipePath = "/tmp/ezshMsgPasser";
-    pipeFile = open(pipePath, O_WRONLY);
+    pipeFile = open(filePath, O_WRONLY);
     write(pipeFile, strtok(msg,"\n"), strlen(msg)+1);
     close(pipeFile);
 }
 
-void pipeRead(void){
+void pipeReadexp(void){
     int pipeFile;
         char pwd[100];
-        char *command = "cd";
-        char * pipePath = "/tmp/ezshMsgPasser";
         while(1){
-            pipeFile = open(pipePath, O_RDONLY);
+            pipeFile = open("/tmp/exp2prompt", O_RDONLY);
             read(pipeFile, pwd, 100);
             printf("%s\n", pwd);
             chdir(pwd);
             close(pipeFile);
-        }
-        
+        }  
 }
+
+// void pipeReadprompt(void){
+//     int pipeFile;
+//         char pwd[100];
+//         char pathy[200];
+//         while(1){
+//             pipeFile = open("/tmp/prompt2exp", O_RDONLY);
+//             read(pipeFile, pwd, 100);
+//             chdir(pwd);
+//             close(pipeFile);
+//         }  
+// }
